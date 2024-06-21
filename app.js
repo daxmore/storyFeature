10c0c1e423366ee9ce58fa3d15bc7bf016e54d3e
daxmore <117888686+daxmore@users.noreply.github.com>
2024-06-21 20:56:41 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,26 @@
+let arr = [{ dp: "https://images.unsplash.com/photo-1718889874468-3a56b84bb2e7?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", story: "https://images.unsplash.com/photo-1718817502686-1fa1c2e8b108?q=80&w=1888&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" },
+{ dp: "https://images.unsplash.com/photo-1718509782458-dfe9c18a7ef8?q=80&w=1964&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", story: "https://images.unsplash.com/photo-1718494119448-16dad33afbed?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" },
+{ dp: "https://images.unsplash.com/photo-1718382341271-070ac4697558?q=80&w=2012&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", story: "https://images.unsplash.com/photo-1715329499545-f57d711c8209?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" },
+{ dp: "https://images.unsplash.com/photo-1702466607907-73c205e3b2c5?q=80&w=1886&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", story: "https://images.unsplash.com/photo-1600607686527-6fb886090705?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" },
+{ dp: "https://images.unsplash.com/photo-1718524127932-2696e2f59096?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", story: "https://images.unsplash.com/photo-1718632831132-c3fa7b1d83a5?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" },
+{ dp: "https://images.unsplash.com/photo-1718526062539-0173bbc35e7f?q=80&w=1935&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", story: "https://images.unsplash.com/photo-1667759317464-b00aa026e408?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" },
+]
+
+let cl = ""
+arr.forEach((e, i) => {
+    cl += `<div class="story">
+                <img src="${e.dp}" alt="" id="${i}"> 
+            </div>`
+});
+
+let str = document.querySelector(".stories")
+let pic = document.querySelector(".picture")
+str.innerHTML = cl
+
+str.addEventListener("click", (e) => {
+    pic.style.display = "block"
+    pic.style.backgroundImage = `url(${arr[e.target.id].story})`
+})
+setInterval(() => {
+    pic.style.display = "none"
+}, 5000);
